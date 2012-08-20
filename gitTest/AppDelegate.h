fa67b805ffb0d9e592417f55cefe0ac2ff914b69
aeiowoo <aeiowoo@gmail.com>
2012-08-20 23:13:50 +0900

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Kookmin Venture Companions on 12. 8. 20..
+//  Copyright (c) 2012 국민대학교. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by Kookmin Venture Companions on 12. 8. 20..
+//  Copyright (c) 2012 국민대학교. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
