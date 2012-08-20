c26844a76cb345f1e5be62e485775d95054da690
aeiowoo <aeiowoo@gmail.com>
2012-08-21 02:16:55 +0900

    test 커밋

---
+++ gitTest/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  gitTest
+//
+//  Created by Kookmin Venture Companions on 12. 8. 21..
+//  Copyright (c) 2012 국민대학교. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface test : UIView
+
+@end
---
+++ gitTest/test.m
@@ -0,0 +1,31 @@
+//
+//  test.m
+//  gitTest
+//
+//  Created by Kookmin Venture Companions on 12. 8. 21..
+//  Copyright (c) 2012 국민대학교. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
